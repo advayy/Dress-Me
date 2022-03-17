c600fc9d95ee2c8ada459c53371ed1a02eb1de48
Advay Rajguru <advayrajguru232000@gmail.com>
2022-03-16 23:53:07 -0700

    Implemented splash screen

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,10 +1,29 @@
 package ui;
 
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
+        JWindow window = new JWindow();
+        window.setSize(new Dimension(800, 500));
+        ImageIcon gif = new ImageIcon("./assets/Clueless_Splash_Screen.gif");
+        JLabel gifL = new JLabel();
+        gifL.setIcon(gif);
+        window.add(gifL);
+        //window.getContentPane().add();
+        window.setVisible(true);
+        window.repaint();
+        window.revalidate();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        window.setVisible(false);
+        window.dispose();
         new DressMeApp();
     }
 }
