c48c5e1625b5fe429515632cab49bb254240951b
Advay Rajguru <advayrajguru232000@gmail.com>
2022-03-19 00:41:34 -0700

    Splash screen resized to match UI

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,33 +1,33 @@
 package ui;
 
-
 import javax.swing.*;
 import java.awt.*;
 
 // Launches app and displays splash screen
 public class Main {
     public static void main(String[] args) {
-        JWindow window = new JWindow();
-        int height = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height;
-        int width = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().width;
-        int locationX = width / 2 - 400;
-        int locationY = height / 2 - 250;
-        window.setLocation(locationX, locationY);
-        window.setSize(new Dimension(800, 500));
-        ImageIcon gif = new ImageIcon("./assets/Clueless_Splash_Screen.gif");
-        JLabel gifL = new JLabel();
-        gifL.setIcon(gif);
-        window.add(gifL);
-        window.setVisible(true);
-        window.repaint();
-        window.revalidate();
+        int userComputerHeight = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height;
+        int userComputerWidth = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().width;
+        int width = 600;
+        int height = 500;
+        int locationX = userComputerWidth / 2 - width / 2;
+        int locationY = userComputerHeight / 2 - height / 2;
+        JWindow splashScreen = new JWindow();
+        splashScreen.setLocation(locationX, locationY);
+        splashScreen.setSize(new Dimension(width, height));
+        JLabel splashGifLabel = new JLabel();
+        splashGifLabel.setIcon(new ImageIcon("./assets/Clueless_Splash_Screen.gif"));
+        splashScreen.add(splashGifLabel);
+        splashScreen.setVisible(true);
+        splashScreen.repaint();
+        splashScreen.revalidate();
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        window.setVisible(false);
-        window.dispose();
+        splashScreen.setVisible(false);
+        splashScreen.dispose();
         new DressMeApp();
     }
 }
