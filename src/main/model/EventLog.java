fa14d45d1f773deaf14ab0f85df5e40266ad8b96
Advay Rajguru <advayrajguru232000@gmail.com>
2022-03-30 23:22:02 -0700

    Checkstyle issue fixed

--- src/main/model/EventLog.java
+++ src/main/model/EventLog.java
@@ -2,57 +2,45 @@ package model;
 
 import java.util.*;
 
-/**
- * Represents a log of alarm system events.
- * We use the Singleton Design Pattern to ensure that there is only
- * one EventLog in the system and that the system has global access
- * to the single instance of the EventLog.
- */
 public class EventLog implements Iterable<Event> {
 	/** the only EventLog in the system (Singleton Design Pattern) */
-	private static EventLog theLog;
-	private Collection<Event> events;
+    private static EventLog theLog;
+    private Collection<Event> events;
 
 	/**
 	 * Prevent external construction.
 	 * (Singleton Design Pattern).
 	 */
-	private EventLog() {
-		events = new ArrayList<Event>();
-	}
+    private EventLog() {
+        events = new ArrayList<Event>();
+    }
 
-	/**
-	 * Gets instance of EventLog - creates it
-	 * if it doesn't already exist.
-	 * (Singleton Design Pattern)
-	 * @return  instance of EventLog
-	 */
-	public static EventLog getInstance() {
-		if (theLog == null) {
-			theLog = new EventLog();
-		}
+    public static EventLog getInstance() {
+        if (theLog == null) {
+            theLog = new EventLog();
+        }
 
-		return theLog;
-	}
+        return theLog;
+    }
 
 	/**
 	 * Adds an event to the event log.
 	 * @param e the event to be added
 	 */
-	public void logEvent(Event e) {
-		events.add(e);
-	}
+    public void logEvent(Event e) {
+    	events.add(e);
+    }
 
 	/**
 	 * Clears the event log and logs the event.
 	 */
-	public void clear() {
-		events.clear();
-		logEvent(new Event("Event log cleared."));
-	}
+    public void clear() {
+    	events.clear();
+    	logEvent(new Event("Event log cleared."));
+    }
 
-	@Override
-	public Iterator<Event> iterator() {
-		return events.iterator();
-	}
+    @Override
+    public Iterator<Event> iterator() {
+    	return events.iterator();
+    }
 }
